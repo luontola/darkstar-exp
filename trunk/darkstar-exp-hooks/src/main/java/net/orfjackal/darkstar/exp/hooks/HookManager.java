e758b1a4dfe75ba2a5b15224dc4ff5b2a6b4041c
esko.luontola <esko.luontola@1344134d-a54c-0410-acda-eb81fd70df80>
2008-07-14 12:17:03 +0000

    [darkstar-exp-hooks] Refactored

--- trunk/darkstar-exp-hooks/src/main/java/net/orfjackal/darkstar/exp/hooks/HookManager.java
+++ trunk/darkstar-exp-hooks/src/main/java/net/orfjackal/darkstar/exp/hooks/HookManager.java
@@ -33,26 +33,27 @@ public class HookManager {
         Object hook = hooks.get(type);
         if (hook == null) {
             // use atomic operations to ensure that only one instance of the hook is ever returned
-            hooks.putIfAbsent(type, createDefaultHook(type));
+            installHook(createDefaultHook(type));
             hook = hooks.get(type);
+            assert hook != null;
         }
         return type.cast(hook);
     }
 
-    private static <T> Object createDefaultHook(Class<T> type) {
-        try {
-            return type.newInstance();
-        } catch (Exception e) {
-            throw new RuntimeException("Unable to create a default hook of type " + type, e);
-        }
-    }
-
-    public void installHook(Object hook) {
+    public void installHook(Hook hook) {
         for (Class<?> type = hook.getClass(); Hook.class.isAssignableFrom(type); type = type.getSuperclass()) {
             Object previous = hooks.putIfAbsent(type, hook);
             if (previous != null) {
                 throw new IllegalArgumentException("A hook of type " + type + " already installed: " + previous);
             }
+        }
+    }
+
+    private static <T extends Hook> T createDefaultHook(Class<T> type) {
+        try {
+            return type.newInstance();
+        } catch (Exception e) {
+            throw new RuntimeException("Unable to create a default hook of type " + type, e);
         }
     }
 }
