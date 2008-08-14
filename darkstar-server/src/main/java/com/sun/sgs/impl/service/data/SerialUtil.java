e00f2feb83c2a3634b3cc9e8e0c525945435e1f7
esko.luontola <esko.luontola@1344134d-a54c-0410-acda-eb81fd70df80>
2008-08-14 15:21:09 +0000

    [darkstar-exp-mods] ReplaceObjectOnSerializationHook

--- darkstar-server/src/main/java/com/sun/sgs/impl/service/data/SerialUtil.java
+++ darkstar-server/src/main/java/com/sun/sgs/impl/service/data/SerialUtil.java
@@ -24,14 +24,10 @@ import com.sun.sgs.app.ManagedReference;
 import com.sun.sgs.app.ObjectIOException;
 import com.sun.sgs.impl.sharedutil.LoggerWrapper;
 import com.sun.sgs.impl.sharedutil.Objects;
-import java.io.ByteArrayInputStream;
-import java.io.ByteArrayOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
-import java.io.ObjectStreamClass;
-import java.io.OutputStream;
+import net.orfjackal.darkstar.exp.hooks.Hooks;
+import net.orfjackal.darkstar.exp.hooks.hooktypes.ReplaceObjectOnSerializationHook;
+
+import java.io.*;
 import java.lang.reflect.Array;
 import java.lang.reflect.Field;
 import java.lang.reflect.Modifier;
@@ -291,7 +287,8 @@ final class SerialUtil {
 
 	/** Check for references to managed objects. */
 	protected Object replaceObject(Object object) throws IOException {
-	    if (object != topLevelObject && object instanceof ManagedObject) {
+            object = Hooks.get(ReplaceObjectOnSerializationHook.class).replaceObject(object, topLevelObject);
+            if (object != topLevelObject && object instanceof ManagedObject) {
 		throw new ObjectIOException(
 		    "ManagedObject was not referenced through a " +
 		    "ManagedReference: " + Objects.safeToString(object),
